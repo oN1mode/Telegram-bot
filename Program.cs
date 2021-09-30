f80f6331642004039109d9587af60954d19aad15
Vyacheslav Fedchishin <slev4a_22_93@mail.ru>
2021-09-30 21:10:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
