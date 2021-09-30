d5cb0f16b6a219aff758c0e8b7cd6c41a329a0ba
Vyacheslav Fedchishin <slev4a_22_93@mail.ru>
2021-09-30 21:25:20 +0300

    created class TelegramBotToken

--- TelegramBotToken.cs
+++ TelegramBotToken.cs
@@ -7,5 +7,39 @@ using System.Threading.Tasks;
 
 namespace Module_9
 {
-    
+    class TelegramBotToken
+    {
+        private string token;
+        public string Token { get; }
+
+        public TelegramBotToken()
+        {
+            this.token = AssigningTokenFromFile();
+        }
+
+        private string AssigningTokenFromFile()
+        {
+            FileInfo file = new FileInfo("token.txt");
+            string token = null;
+            if (file.Exists)
+            {
+                using (StreamReader sr = new StreamReader(file.FullName))
+                {
+                        token = sr.ReadToEnd();
+                }
+                if (token == null)
+                {
+                    Console.WriteLine($"В файле {file.FullName} отсутствует token!");
+                    Console.ReadKey();
+                }
+                return token;
+            }
+            else
+            {
+                Console.WriteLine($"Файл token.txt не сущществует, пожалуйста создайте его и запишите в него token");
+                return token;
+            }
+        }
+
+    }
 }
